750ca2365a0d87108cfd2f728090721a99175140
goldyasce <trubin.v.a2015@gmail.com>
2021-10-01 16:45:11 +0300

    Add mustache lib

--- blog/src/main/java/com/goldyasce/blog/controller/BlogController.java
+++ blog/src/main/java/com/goldyasce/blog/controller/BlogController.java
@@ -2,17 +2,19 @@ package com.goldyasce.blog.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 public class BlogController {
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+                        Map<String, Object> model) {
+        model.put("name", name);
+
         return "hello";
     }
 
-    @GetMapping("/")
-    public String main() {
-        return "main";
-    }
 }
